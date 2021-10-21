10663edec1144dd4644e451611d433f2fd075258
Divya S Gupta <dg6219@srmist.edu.in>
2021-10-21 18:17:18 +0530

    Update script.js

--- coursera_mod4/script.js
+++ coursera_mod4/script.js
@@ -2,20 +2,23 @@ var names=new Array();
 names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
+names[3]="jhalak";
+names[4]="sam";
 names[5]="frank";
-names[6]="larry";
+names[6]="harry";
 names[7]="paula";
 names[8]="laura";
-names[9]="jim";
+names[9]="jimmy";
 
 
-for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+for (var i = 0; i < names.length; i++)
+{
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j')
+	{
         console.log("Goodbye "+ names[i])
 	}
-	else{
+	else
+	{
 		console.log("Hello "+ names[i])
 	}
 }
